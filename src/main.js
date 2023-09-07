b8206446da7b590c9554d7db803d0b822acb1e3b
Oscar <oscar.jcarabali@gmail.com>
2023-09-07 00:01:10 -0500

    validation input email

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-const boton = document.querySelector('button');
+const boton = document.querySelector('#button');
+const form = document.querySelector('#form');
+const inputEmail = document.querySelector("#email")
+const labelEmail = document.querySelector("#labelEmail")
 
-boton.addEventListener('click', changeColor);
+form.addEventListener('submit', changeColor)
 
-function changeColor (element) {
-    element.target.classList.toggle("background-color")
+function changeColor (event) {
+    //console.log("lorem*fadkjsfl;kajdf;laj dslfjadslkfjadsl;kjfalkdsjf;alkdjf ;lkadsjfl;kasdjf;lkasdj;l");
+    const valiudEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if(valiudEmail.test(inputEmail.value)) {
+        console.log("send");
+    } else {
+        event.preventDefault();
+        console.log("no send");
+        inputEmail.classList.add("invalid-input")
+        labelEmail.classList.add("labelEmail")
+    }
 }
